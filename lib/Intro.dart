07fe06100efa86aff162471dc90933b91beee1bc
Maksim <maksalmak@gmail.com>
2019-07-28 05:44:30 +0300

    Dome merge

--- lib/Intro.dart
+++ lib/Intro.dart
@@ -10,126 +10,237 @@ class App extends StatelessWidget {
   //making list of pages needed to pass in IntroViewsFlutter constructor.
   final pages = [
     PageViewModel(
-        pageColor: const Color(0xFF03A9F4),
-        // iconImageAssetPath: 'assets/air-hostess.png',
-        // bubble: Image.asset('assets/air-hostess.png'),
-        body: Text(
-          'Проходите уровни и узновайте что-то новое о кибербезопасности каждый день',
-        ),
-        title: Text(''),
-        textStyle:
-            TextStyle(fontFamily: 'MyFont', color: Colors.white, fontSize: 18),
-        mainImage: Column(
-          children: <Widget>[
-            Expanded(
-              flex: 3, // 20%
-              child: Text(''),
+      pageColor: const Color(0xFF03A9F4),
+      // iconImageAssetPath: 'assets/air-hostess.png',
+      // bubble: Image.asset('assets/air-hostess.png'),
+      body: Text(
+        'Проходите уровни и узновайте что-то новое о кибербезопасности каждый день',
+      ),
+      title: Text(''),
+      textStyle:
+          TextStyle(fontFamily: 'MyFont', color: Colors.white, fontSize: 18),
+      mainImage: Stack(
+        children: <Widget>[
+          Align(
+            child: SvgPicture.asset(
+              'assets/SafeHack_Logo.svg',
+              height: 150.0,
+              width: 150.0,
             ),
-            Expanded(
-              flex: 8, // 20%
-              child: Row(
-                children: <Widget>[
-                  Expanded(
-                    flex: 4, // 20%
-                    child: Align(
-                      child: SvgPicture.asset(
-                        'assets/SafeHack_Logo.svg',
-                        height: 180.0,
-                        width: 180.0,
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    flex: 3, // 20%
-                    child: Align(
-                      child: Text(
-                        'Safe \n Hack',
-                        style: TextStyle(
-                            fontFamily: 'MyFont',
-                            color: Colors.white,
-                            fontSize: 50),
-                        textAlign: TextAlign.center,
-                      ),
-                      alignment: Alignment.center,
-                    ),
-                  ),
-                ],
-              ),
+            alignment: new Alignment(-0.50, 0),
+          ),
+          Align(
+            child: Text(
+              'Safe \n Hack',
+              style: TextStyle(
+                  fontFamily: 'MyFont', color: Colors.white, fontSize: 50),
+              textAlign: TextAlign.center,
             ),
-            Expanded(
-              flex: 2, // 20%
-              child: Align(
-                child: Text(
-                  'О киберугрозах - безопасно!',
-                  style: TextStyle(
-                      fontFamily: 'MyFont', color: Colors.white, fontSize: 30),
-                  textAlign: TextAlign.center,
-                ),
-              ),
+            alignment: new Alignment(0.55, 0),
+          ),
+          Align(
+            child: Text(
+              'Повышай уровень знаний - повышай рейтинг!',
+              style: TextStyle(
+                  fontFamily: 'MyFont', color: Colors.white, fontSize: 24),
+              textAlign: TextAlign.center,
             ),
-          ],
-        )),
+            alignment: new Alignment(0, 1.3),
+          ),
+        ],
+      ),
+    ),
     PageViewModel(
       pageColor: const Color(0xFF8BC34A),
       body: Text(
-        'We  work  for  the  comfort ,  enjoy  your  stay  at  our  beautiful  hotels',
+        'Изучайте компьютерную грамотность, криптографию и основы иноформативной безопасности',
+        style: TextStyle(fontSize: 18),
       ),
-      title: Text('Hotels'),
-      // mainImage: Image.asset(
-      //   'assets/hotel.png',
-      //   height: 285.0,
-      //   width: 285.0,
-      //   alignment: Alignment.center,
-      // ),
+      title: Text(''),
+      mainImage: Stack(
+        children: <Widget>[
+          Align(
+            child: SvgPicture.asset(
+              'assets/computer.svg',
+              height: 285.0,
+              width: 285.0,
+              //alignment: Alignment.center,
+            ),
+            alignment: new Alignment(0, 0),
+          ),
+          Align(
+            child: SvgPicture.asset(
+              'assets/sword.svg',
+              height: 100.0,
+              width: 100.0,
+            ),
+            alignment: new Alignment(-0.6, 0.7),
+          ),
+          Align(
+            child: SvgPicture.asset(
+              'assets/SafeHack_Logo.svg',
+              height: 100.0,
+              width: 100.0,
+            ),
+            alignment: new Alignment(-0.3, -1.5),
+          ),
+          Align(
+            child: Text(
+              'Safe \n Hack',
+              style: TextStyle(
+                  fontFamily: 'MyFont', color: Colors.white, fontSize: 30),
+              textAlign: TextAlign.center,
+            ),
+            alignment: new Alignment(0.3, -1.4),
+          ),
+          Align(
+            child: Text(
+              'Два режима игры!\nЛучшая защита - это нападение.',
+              style: TextStyle(
+                  fontFamily: 'MyFont', color: Colors.white, fontSize: 24),
+              textAlign: TextAlign.center,
+            ),
+            alignment: new Alignment(0, 1.3),
+          ),
+        ],
+      ),
       textStyle: TextStyle(fontFamily: 'MyFont', color: Colors.white),
     ),
     PageViewModel(
       pageColor: const Color(0xFF607D8B),
       body: Text(
-        'Easy  cab  booking  at  your  doorstep  with  cashless  payment  system',
+        'Только полезная информация. Обучение от простого к сложному на интерактивных примерах и логических задачах.',
+        style: TextStyle(fontSize: 18),
       ),
-      title: Text('Cabs'),
-      // mainImage: Image.asset(
-      //   'assets/taxi.png',
-      //   height: 285.0,
-      //   width: 285.0,
-      //   alignment: Alignment.center,
-      // ),
+      title: Text(''),
+      mainImage: Stack(
+        children: <Widget>[
+          Align(
+            child: SvgPicture.asset(
+              'assets/flowchart.svg',
+              height: 150.0,
+              width: 150.0,
+              //alignment: Alignment.center,
+            ),
+            alignment: new Alignment(-0.6, 0.2),
+          ),
+          Align(
+            child: SvgPicture.asset(
+              'assets/debugging.svg',
+              height: 150.0,
+              width: 150.0,
+            ),
+            alignment: new Alignment(0.6, 0.8),
+          ),
+          Align(
+            child: SvgPicture.asset(
+              'assets/SafeHack_Logo.svg',
+              height: 100.0,
+              width: 100.0,
+            ),
+            alignment: new Alignment(-0.3, -1.5),
+          ),
+          Align(
+            child: Text(
+              'Safe \n Hack',
+              style: TextStyle(
+                  fontFamily: 'MyFont', color: Colors.white, fontSize: 30),
+              textAlign: TextAlign.center,
+            ),
+            alignment: new Alignment(0.3, -1.4),
+          ),
+          Align(
+            child: Text(
+              'Актуальные вопросы кибербезопасности!',
+              style: TextStyle(
+                  fontFamily: 'MyFont', color: Colors.white, fontSize: 24),
+              textAlign: TextAlign.center,
+            ),
+            alignment: new Alignment(0, 1.3),
+          ),
+        ],
+      ),
       textStyle: TextStyle(fontFamily: 'MyFont', color: Colors.white),
     ),
     PageViewModel(
-      pageColor: const Color(0xFF607D8B),
+      pageColor: const Color(0xFF8BC34A),
       body: Text(
-        'Easy  cab  booking  at  your  doorstep  with  cashless  payment  system',
+        'Соревнуйся с друзьсями, зарабатывай BIGcoin\'ы, используйте подсказкиб делая свою жизнь в интернете безопаснее!',
+        style: TextStyle(fontSize: 18),
       ),
-      title: Text('Cabs'),
-      // mainImage: Image.asset(
-      //   'assets/taxi.png',
-      //   height: 285.0,
-      //   width: 285.0,
-      //   alignment: Alignment.center,
-      // ),
+      title: Text(''),
+      mainImage: Stack(
+        children: <Widget>[
+          Align(
+            child: SvgPicture.asset(
+              'assets/bars.svg',
+              height: 250.0,
+              width: 250.0,
+              //alignment: Alignment.center,
+            ),
+            alignment: new Alignment(0, 0),
+          ),
+          Align(
+            child: SvgPicture.asset(
+              'assets/coin.svg',
+              height: 50.0,
+              width: 50.0,
+            ),
+            alignment: new Alignment(-0.1, -0.5),
+          ),
+          Align(
+            child: SvgPicture.asset(
+              'assets/coin.svg',
+              height: 50.0,
+              width: 50.0,
+            ),
+            alignment: new Alignment(0.6, -0.8),
+          ),
+          Align(
+            child: SvgPicture.asset(
+              'assets/SafeHack_Logo.svg',
+              height: 100.0,
+              width: 100.0,
+            ),
+            alignment: new Alignment(-0.3, -1.5),
+          ),
+          Align(
+            child: Text(
+              'Safe \n Hack',
+              style: TextStyle(
+                  fontFamily: 'MyFont', color: Colors.white, fontSize: 30),
+              textAlign: TextAlign.center,
+            ),
+            alignment: new Alignment(0.3, -1.4),
+          ),
+          Align(
+            child: Text(
+              'Повышай уровень знаний - повышай рейтинг!',
+              style: TextStyle(
+                  fontFamily: 'MyFont', color: Colors.white, fontSize: 24),
+              textAlign: TextAlign.center,
+            ),
+            alignment: new Alignment(0, 1.3),
+          ),
+        ],
+      ),
       textStyle: TextStyle(fontFamily: 'MyFont', color: Colors.white),
     ),
   ];
 
   @override
   Widget build(BuildContext context) {
-    return IntroViewsFlutter(
-      pages,
-      onTapDoneButton: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => PasswordAttackPage(
-                Person(name: 'Alice', birthDay: '07 июля 2002')),
-          ),
-        );
-      },
-      pageButtonTextStyles: TextStyle(
-        color: Colors.white,
-        fontSize: 18.0,
-      ),
-    );
+    return IntroViewsFlutter(pages, onTapDoneButton: () {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => PasswordAttackPage(Person(name: 'Аня Иванова')),
+        ), //MaterialPageRoute
+      );
+    },
+        pageButtonTextStyles: TextStyle(
+          color: Colors.white,
+          fontSize: 18.0,
+        ));
   }
 }
