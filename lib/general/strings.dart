ae0fe5bbd140b68aa7e7cc8a162d2714d40b54b6
Mikhail <wowivan2011@yandex.ru>
2019-07-28 09:11:34 +0300

    Add change level mode

--- lib/general/strings.dart
+++ lib/general/strings.dart
@@ -3,6 +3,7 @@ final fontFamilyLight = 'Helvetica';
 
 final girlPic = 'assets/girl.svg';
 final avatarPic = 'assets/avatar.svg';
+final globusPic = 'assets/earth-globe.svg';
 final settingPic = 'assets/settings.svg';
 final playerButtonPic = 'assets/play-button.svg';
 final backPic = 'assets/back.svg';
@@ -11,5 +12,7 @@ final coinPic = 'assets/coin.svg';
 final goalPic = 'assets/goal.svg';
 final lockPic = 'assets/locked.svg';
 final bugPic = 'assets/virus.svg';
+final shildPic = 'assets/shield_recolored.svg';
+final swordPic = 'assets/sword.svg';
 
 final imagePath = 'assets/SafeHack_Logo.svg';
